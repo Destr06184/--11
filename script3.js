e4b65f792c639d0d8eef268b8aea9170e9753be5
KFG <KFG>
2026-02-23 16:03:41 +0300

    пр11

--- script3.js
+++ script3.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+      document.addEventListener('DOMContentLoaded', () => {
     const items = document.querySelectorAll('.item');
 
     const activeItem = document.querySelector('.item.active');
